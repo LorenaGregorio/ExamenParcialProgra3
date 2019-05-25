5ae7ee27d35199b5f68a8ca98c526c16dcc4d45f
Lorena Gregorio Hernandez <lorenaghernandez@hotmail.com>
2019-05-25 17:34:54 -0600

    l

--- ExamenParcialProgra3/Form1.Designer.cs
+++ ExamenParcialProgra3/Form1.Designer.cs
@@ -43,6 +43,8 @@
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.textBox6 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
@@ -169,12 +171,22 @@
             this.button1.TabIndex = 14;
             this.button1.Text = "Agregar";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(293, 122);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(478, 209);
+            this.dataGridView1.TabIndex = 15;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox6);
             this.Controls.Add(this.textBox5);
@@ -192,6 +204,8 @@
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -214,6 +228,7 @@
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.TextBox textBox6;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
 
--- ExamenParcialProgra3/Form1.cs
+++ ExamenParcialProgra3/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ExamenParcialProgra3.Clases;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,65 @@ namespace ExamenParcialProgra3
 {
     public partial class Form1 : Form
     {
+        // Se crea el objeto conexion para que se obtegan los datos
+        Conexion con = new Conexion();
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //llamo al metodo actualizargrid
+            this.ActualizarGrid();
+        }
+
+
+        // Metodo para actualizar datagridview
+        public void ActualizarGrid()
+        {
+            // Aca se hace una funcion (select) para mostrar los datos
+            con.ActualizarGrid(this.dataGridView1, "Select * from DatosTBL");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //se crea el objeto dato
+
+
+            Datos d = new Datos
+           (
+                Convert.ToInt32(textBox1.Text),
+                textBox2.Text,
+                comboBox1.Text,
+                textBox3.Text,
+                textBox4.Text,
+                textBox5.Text,
+                textBox6.Text
+               
+           );
+
+
+
+
+            con.Conectar();
+
+            //Se crea una consulta para insertar los datos (Guardar)
+            string consulta = "insert into DatosTBL (Id,NombreEmpleado,TipodeEquipo,Marca,Procesador,DiscoDuro,SistemaOperativo) " +
+                "values (" + d.id + ", '" + d.nombreempleado + "','" + d.tipodeequipo + "','" + d.marca + "','" + d.procesador + "'," + d.discoduro + ",'" + d.sistemaoperativo + "' );";
+
+            //con esta funcion ejecuto la consulta de arriba en codigo sql
+            con.EjecutarSql(consulta);
+            this.ActualizarGrid();
+            con.Desconectar();
+
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            
+
         }
     }
 }
